aa31c92bfc0bb6421234bcdfcbdf1a46738c143d
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-04-13 17:43:54 +0530

    change in file

--- demon_checker.py
+++ demon_checker.py
@@ -5,15 +5,16 @@ Created on Thu Apr 13 14:17:39 2023
 
 @author: gowrav
 """
-import os
+import time
 import subprocess
 from subprocess import PIPE, Popen
+from prefect import task, flow
 
 os.chdir('/home/gowrav')
 
 
-
-def something():  
+@task
+def check_status():  
     state = 'FAULT STATE'
     terminal  =  Popen(['systemctl','status','keepalived.service'], 
                                   stdout=PIPE,
@@ -22,7 +23,22 @@ def something():
     decoded_stdout = stdout.decode()
     decoded_stderr = stderr.decode()
     if 'MASTER STATE' in decoded_stdout:
-        terminal='MASTER STATE'
-    return terminal
+        state='MASTER STATE'
+    return state
 
-print(something())
+
+@flow
+def trigger():
+    status = check_status()
+    if status == "MASTER STATE":
+        print('entered into master state block')
+        time.sleep(10)
+        return 
+    print('entered into fault state block')
+    time.sleep(5)
+    return 
+
+
+            
+
+
