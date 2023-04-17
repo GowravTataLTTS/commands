d0f723d673e8609d00b0c7b7d7aa890d25f5b8b5
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-04-17 19:51:38 +0530

    all

--- all_tasks.py
+++ all_tasks.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr 17 17:47:18 2023
+Created on Mon Apr 17 19:45:04 2023
 
 @author: gowrav
 """
-#from prefect import task
+
 from pgsql_connector import SQL
 import time
 from  pgsql_connector import Customers
+from prefect import task, flow
+from subprocess import PIPE, Popen
+import schedule
+from multiprocessing import Process
+from datetime import datetime
 sql = SQL()
 
+@task
+def keepalived_status():  
+    state = 'FAULT STATE'
+    terminal  =  Popen(['systemctl','status','keepalived.service'], 
+                                  stdout=PIPE,
+                                  stderr=PIPE)
+    stdout,stderr=terminal.communicate()
+    if stdout.decode().lower().split()[-2]=='master':
+        state='MASTER STATE'
+    return state
 
-
+@task
 def retrive_data():
     with sql.transaction() as session:
         return session.execute('SELECT * FROM CUSTOMERS').all()
 
-
+@task
 def transformation_one(data):
     country_map = {"USA":"United States","IND":"India",
                    "UK":"United Kingdom",
@@ -26,15 +41,21 @@ def transformation_one(data):
         i['country'] = country_map[i['country']]
         i['name'] = '1'+ ' '+ i['name'].lower()
     time.sleep(5)
+    print('first transformation is done')
+    print(data)
     return data
 
+@task
 def transformation_two(data):
     for i in data:
         i['age']= str(i['age'])+''+'Middle Age' if  i['age']>40 else str(i['age'])+' '+'Young Age'
         i['name'] =i['name'].replace('1','2')
     time.sleep(5)
+    print('second transformation is done')
+    print(data)
     return data
 
+@task
 def transformation_three(data):
     country_map = {"United States":"American","India":"Indian",
                    "United Kingdom":"English",
@@ -43,10 +64,11 @@ def transformation_three(data):
         i['country'] = country_map[i['country']]
         i['name'] =i['name'].replace('2','3')
     time.sleep(5)
+    print('third transformation is done')
+    print(data)
     return data
 
-
-
+@task
 def insert_data(data):
     with sql.transaction() as session:
         session.bulk_update_mappings(Customers, data)
---
+++ prefect_with_python.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 17:47:18 2023
+
+@author: gowrav
+"""
+from pgsql_connector import SQL
+from prefect import task, flow
+from all_tasks import keepalived_status,retrive_data,transformation_one, transformation_two,transformation_three,insert_data
+import schedule
+from multiprocessing import Process
+from datetime import datetime
+sql = SQL()
+        
+        
+
+#@flow
+def trigger():  
+    names = retrive_data()
+    all_names = transformation_one(names)
+    second_names = transformation_two(all_names)
+    third_names = transformation_three(second_names)
+    insert_data(third_names)
+    return
+
+
+def prefect_checker(): 
+    status = keepalived_status()
+    if status == "MASTER STATE":
+        trigger()
+    else:
+        print(datetime.now().strftime("%H:%M:%S"), "BACKUP")
+    
+
+def run_per_time():
+    schedule.every(1).minute.do(prefect_checker)
+    while True:
+        schedule.run_pending()
+
+
+
+
+def run_job():
+    p=Process(target=run_per_time)
+    p.start()
+    p.join()
+    
+    
+if __name__=='__main__':
+    run_job()
