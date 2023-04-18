bba3955da32253741cd20f654832fae4acf002aa
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-04-18 10:32:45 +0530

    changed as per the requirement

--- all_tasks.py
+++ all_tasks.py
@@ -8,67 +8,77 @@ Created on Mon Apr 17 19:45:04 2023
 
 from pgsql_connector import SQL
 import time
-from  database_connections import Customers
+from database_connections import Customers
 from prefect import task, flow
 from subprocess import PIPE, Popen
 import schedule
 from multiprocessing import Process
 from datetime import datetime
+
 sql = SQL()
 
-#@task
-def keepalived_status():  
+
+# @task
+def keepalived_status():
     state = 'FAULT STATE'
-    terminal  =  Popen(['systemctl','status','keepalived.service'], 
-                                  stdout=PIPE,
-                                  stderr=PIPE)
-    stdout,stderr=terminal.communicate()
-    if stdout.decode().lower().split()[-2]=='master':
-        state='MASTER STATE'
+    terminal = Popen(['systemctl', 'status', 'keepalived.service'],
+                     stdout=PIPE,
+                     stderr=PIPE)
+    stdout, stderr = terminal.communicate()
+    if stdout.decode().lower().split()[-2] == 'master':
+        state = 'MASTER STATE'
     return state
 
-#@task
+
+# @task
 def retrive_data():
     with sql.transaction() as session:
         return session.execute('SELECT * FROM CUSTOMERS').all()
 
-#@task
+
+# @task
 def transformation_one(data):
-    country_map = {"USA":"United States","IND":"India",
-                   "UK":"United Kingdom",
-                   "AUS":"Australia"}
+    country_map = {"USA": "United States", "IND": "India",
+                   "UK": "United Kingdom",
+                   "AUS": "Australia"}
     for i in data:
-        i['country'] = country_map[i['country']]
-        i['name'] = '1'+ ' '+ i['name'].lower()
+        if i['country'] in country_map.keys():
+            i['country'] = country_map[i['country']]
+            i['name'] = '1' + ' ' + i['name'].lower()
     time.sleep(5)
     print('first transformation is done')
     print(data)
     return data
 
-#@task
+
+# @task
 def transformation_two(data):
     for i in data:
-        i['age']= str(i['age'])+''+'Middle Age' if  i['age']>40 else str(i['age'])+' '+'Young Age'
-        i['name'] =i['name'].replace('1','2')
+        if isinstance(i['age'], int):
+            i['age'] = str(i['age']) + '' + 'Middle Age' if i['age'] > 40 else str(i['age']) + ' ' + 'Young Age'
+            i['name'] = i['name'].replace('1', '2')
     time.sleep(5)
     print('second transformation is done')
     print(data)
     return data
 
-#@task
+
+# @task
 def transformation_three(data):
-    country_map = {"United States":"American","India":"Indian",
-                   "United Kingdom":"English",
-                   "Australia":"Aussie"}
+    country_map = {"United States": "American", "India": "Indian",
+                   "United Kingdom": "English",
+                   "Australia": "Aussie"}
     for i in data:
-        i['country'] = country_map[i['country']]
-        i['name'] =i['name'].replace('2','3')
+        if i['country'] in country_map.keys():
+            i['country'] = country_map[i['country']]
+            i['name'] = i['name'].replace('2', '3')
     time.sleep(5)
     print('third transformation is done')
     print(data)
     return data
 
-#@task
+
+# @task
 def insert_data(data):
     with sql.transaction() as session:
         session.bulk_update_mappings(Customers, data)
--- support/task_tester.py
+++ support/task_tester.py
@@ -6,48 +6,51 @@ Created on Mon Apr 17 18:55:22 2023
 @author: gowrav
 """
 import random
-first_name = ['Ayan', "Brad", "Chetan", "David","Eswar"]
-last_name = ["Reddy","Smith","Naidu","Abbas","Jenkovich"]
 
-
+first_name = ['Ayan', "Brad", "Chetan", "David", "Eswar"]
+last_name = ["Reddy", "Smith", "Naidu", "Abbas", "Jenkovich"]
 
 names = []
 for i in range(5):
-    country = ['IND','USA','UK', 'AUS']
-    name =random.choice(first_name) + ' ' + random.choice(last_name)
+    country = ['IND', 'USA', 'UK', 'AUS']
+    name = random.choice(first_name) + ' ' + random.choice(last_name)
     country = random.choice(country)
-    age = random.choice(list(range(18,61)))
+    age = random.choice(list(range(18, 61)))
 
-    new_names = {'name':name,
-                 'age':age,
-                 'country':country}
+    new_names = {'name': name,
+                 'age': age,
+                 'country': country}
     names.append(new_names)
 
+
 def transformation_one(data):
-    country_map = {"USA":"United States","IND":"India",
-                   "UK":"United Kingdom",
-                   "AUS":"Australia"}
+    country_map = {"USA": "United States", "IND": "India",
+                   "UK": "United Kingdom",
+                   "AUS": "Australia"}
     for i in data:
-        i['country'] = country_map[i['country']]
-        i['name'] =i['name'].replace('1','2')
+        if i['country'] in country_map.keys():
+            i['country'] = country_map[i['country']]
+            i['name'] = i['name'].replace('1', '2')
     return data
+
 
 def transformation_two(data):
     for i in data:
-        i['age']= str(i['age'])+''+'Middle Age' if  i['age']>40 else str(i['age'])+' '+'Young Age'
-        i['name'] =i['name'].replace('2','3')
+        if isinstance(i['age'], int):
+            i['age'] = str(i['age']) + ' ' + 'Middle Age' if i['age'] > 40 else str(i['age']) + ' ' + 'Young Age'
+            i['name'] = i['name'].replace('2', '3')
     return data
+
 
 def transformation_three(data):
-    country_map = {"United States":"American","India":"Indian",
-                   "United Kingdom":"English",
-                   "Australia":"Aussie"}
+    country_map = {"United States": "American", "India": "Indian",
+                   "United Kingdom": "English",
+                   "Australia": "Aussie"}
     for i in data:
-        i['country'] = country_map[i['country']]
-        i['name'] = '3'+ ' '+ i['name'].lower()
+        if i['country'] in country_map.keys():
+            i['country'] = country_map[i['country']]
+            i['name'] = '3' + ' ' + i['name'].lower()
     return data
-
-
 
 
 all_names = transformation_one(names)
