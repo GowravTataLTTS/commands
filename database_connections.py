10db9a3d4295bfba5605260055b9edf650d8d1f4
GowravTataLTTS <gowrav.bhargav@ltts.com>
2023-04-19 09:58:42 +0530

    new commit

--- database_connections.py
+++ database_connections.py
@@ -6,9 +6,6 @@ Created on Mon Apr 17 17:04:04 2023
 @author: gowrav
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
-
 from sqlalchemy import Column, MetaData, Text, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -18,30 +15,6 @@ hostname = "localhost"
 database_name = "customers"
 user = "postgres"
 password = "postgres"
-
-
-#class SQL:
-#    def __init__(self, **kwargs):
-#        self.engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{hostname}:5432/{database_name}')
-#        self.sessionfactory = sessionmaker(bind=self.engine)
-#
-#    def transaction_one(self):
-#        engine = create_engine(f'postgresql+psycopg2://{user}:{password}@{hostname}:5432/{database_name}')
-#        sessionfactory = sessionmaker(bind=engine)
-#        session = sessionfactory()
-#        return session
-#
-#    def transaction(self):
-#        session = scoped_session(self.sessionfactory)
-#        try:
-#            yield session
-#            session.commit()
-#        except Exception:
-#            session.rollback()
-#            raise
-#        finally:
-#            session.close()
-
 
 Base = declarative_base(metadata=metadata)
 
